b3a17e8dabe61b95c4950194b01ed7b5ac3ce345
dima1233434 <dimaipatii1994@gmail.com>
2022-01-21 18:35:09 +0100

    Added a Procfile.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e0903c1a0750ee593d3695c368ebba2c360cfe5f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e0903c1a0750ee593d3695c368ebba2c360cfe5f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
