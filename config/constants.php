89c2901c54208d3bd63849ff2d65f6d0d8ca3635
Dmytro Ipatii <87619572+monkeyking123g@users.noreply.github.com>
2022-01-23 13:57:48 +0100

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -3,11 +3,11 @@
     session_start();
 
     //Create Constants to Store Non Repeating Values
-    define('SITEURL','http://localhost/food-order/' );
+    define('SITEURL','https://food-order-php.000webhostapp.com/' );
     define('LOCALHOST', 'localhost');
-    define('DB_USERNAME','root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('DB_USERNAME','id18331501_dimaipatii');
+    define('DB_PASSWORD', 'Ppm.YaGwg@D8Myx');
+    define('DB_NAME', 'id18331501_foodorderphp');
 
 
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error($conn)); //database connection
